5f3e29ae7c31870aa2c47d9aa74e76d19e82509f
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2024-08-04 17:51:45 +0530

    copy card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import Card from "./Components/Card.jsx";
 import Select from "./Components/Select.jsx";
+import Active from './Components/Active.jsx' 
 import { RoundProvider } from "./Context/RoundContext.jsx";
+import { ActiveProvider } from "./Context/ActiveContext.jsx";
 
 export default function App() {
 
   return (
+    <ActiveProvider>
     <RoundProvider>
       <div className="flex justify-between m-auto max-w-4xl p-5">
         <Select/>
         <Card />
+        <Active/>
       </div>
     </RoundProvider>
+    </ActiveProvider>
   );
 }
