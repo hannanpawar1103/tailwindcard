cf06d1426cf25fc477d4001769ad43e59f48d02d
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2024-08-04 17:50:09 +0530

    copy card

--- src/Context/RoundContext.jsx
+++ src/Context/RoundContext.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createContext , useContext ,useState } from 'react'
 
 const RoundContext = createContext();
+const ActiveContext = createContext();
 
 export function RoundProvider({ children }) {
   const [round, setround] = useState('none');
@@ -13,6 +14,23 @@ export function RoundProvider({ children }) {
   );
 }
 
+export function ActiveProvider({ children }) {
+  const [active, setactive] = useState(false);
+  return (
+    <ActiveContext.Provider value={{ active, setactive }}>
+      {children}
+    </ActiveContext.Provider>
+  );
+
+}
+
 export function useRound(){
     return useContext(RoundContext)
+
 }
+
+export function useActive(){
+    return useContext(ActiveContext)
+}
+
+
